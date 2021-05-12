938f9d2ce5a4d6ceb3a8a32e481836ec9b785706
siddhantk232 <siddhantk232@gmail.com>
2021-05-12 21:12:26 +0530

    feat: gulp setup
    nunjucks templates for html
    projects.json config file
    on demand tailwindcss

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest, parallel, watch } = require("gulp");
+const nunjucks = require("gulp-nunjucks-html");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const data = require("gulp-data");
+const fs = require("fs");
+
+function templates() {
+  return src("src/**/*.html")
+    .pipe(
+      data(function () {
+        return JSON.parse(fs.readFileSync("./projects.json"));
+      })
+    )
+    .pipe(nunjucks())
+    .pipe(dest("_site"));
+}
+
+function buildSass() {
+  return src("src/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss())
+    .pipe(dest("_site"));
+}
+
+exports.default = parallel(templates, buildSass);
+exports.watch = watch(
+  ["src/**/*.html", "gulpfile.js", "src/**/*.js", "src/**/*.scss"],
+  parallel(templates, buildSass)
+);
