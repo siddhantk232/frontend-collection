a357ac7ac8d227024d1d6403b9bb87da260ff4e8
siddhantk232 <siddhantk232@gmail.com>
2021-05-13 13:20:31 +0530

    fix: gulp watch fixes
    and some other gulp errors
    tailwindcss test

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,84 @@
-const { src, dest, parallel, watch } = require("gulp");
-const nunjucks = require("gulp-nunjucks-html");
+const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
+const browserSync = require("browser-sync");
 const data = require("gulp-data");
+const nunjucks = require("gulp-nunjucks-html");
 const fs = require("fs");
 
-function templates() {
-  return src("src/**/*.html")
+const config = {
+  scss: "src/**/*.scss",
+  html: "src/**/*.html",
+  js: "src/**/*.js",
+  output: "_site",
+};
+
+const buildStyles = function () {
+  // Run tasks on all Sass files
+  return src(config["scss"])
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+        sourceComments: true,
+      })
+    )
+    .pipe(postcss())
+    .pipe(dest(config["output"]));
+};
+
+// Copy static files into output folder
+const javscript = function () {
+  // Copy static files
+  return src(config["js"]).pipe(dest(config["output"]));
+};
+
+const templates = function () {
+  return src(config["html"])
     .pipe(
       data(function () {
         return JSON.parse(fs.readFileSync("./projects.json"));
       })
     )
     .pipe(nunjucks())
-    .pipe(dest("_site"));
-}
+    .pipe(dest(config["output"]));
+};
 
-function buildSass() {
-  return src("src/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postcss())
-    .pipe(dest("_site"));
-}
+// Watch for changes to the src directory
+const startServer = function (done) {
+  // Initialize BrowserSync
+  browserSync.init({
+    server: {
+      baseDir: config["output"],
+    },
+  });
 
-exports.default = parallel(templates, buildSass);
-exports.watch = watch(
-  [
-    "src/**/*.html",
-    "gulpfile.js",
-    "src/**/*.js",
-    "src/**/*.scss",
-    "projects.json",
-  ],
-  parallel(templates, buildSass)
-);
+  // Signal completion
+  done();
+};
+
+// Reload the browser when files change
+const reloadBrowser = function (done) {
+  browserSync.reload();
+  done();
+};
+
+// Watch for changes
+const watchSource = function (done) {
+  watch(
+    [config.html, config.scss, config.js, "gulpfile.js", "projects.json"],
+    series(exports.default, reloadBrowser)
+  );
+  done();
+};
+
+/**
+ * Export Tasks
+ */
+
+// Default task
+// gulp
+exports.default = parallel(buildStyles, javscript, templates);
+
+// Watch and reload
+// gulp watch
+exports.watch = series(exports.default, startServer, watchSource);
